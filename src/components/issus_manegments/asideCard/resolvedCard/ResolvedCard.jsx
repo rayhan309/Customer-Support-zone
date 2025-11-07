46d2f0b7b821c9b51bad19aea11f6d72afabdecf
Abu Rayhan <ihaveawonderfull@gmail.com>
2025-11-06 20:10:02 -1200

    add to m

--- src/components/issus_manegments/asideCard/resolvedCard/ResolvedCard.jsx
+++ src/components/issus_manegments/asideCard/resolvedCard/ResolvedCard.jsx
@@ -5,7 +5,7 @@ const ResolvedCard = ({resolvedData}) => {
     return (
         <div className="px-3 gFont py-3 mt-4 bg-white hover:bg-gray-200 rounded-lg">
             <h2 className='text-lg font-medium text-[#001931]'>{resolvedData.title}</h2>
-            <div className="flex justify-between items-center mr-4">
+            <div className="flex justify-between items-center mr-4 mt-3">
                 <h4 className="text-[14px] font-medium text-[#00ff73]"><i class="fa-regular fa-circle-check"></i> Complited</h4>
                 <p className="text-[14px] font-normal text-gray-700">{`${nowDate.toLocaleTimeString()}`}</p>
             </div> 
